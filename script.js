6e33c8b31a385c26c531ca2a47cc56aa1558821a
Qua Nguyen <Nguyenmqua@gmail.com>
2020-07-31 10:47:08 -0600

    got the functionality to work properly

--- script.js
+++ script.js
@@ -11,18 +11,17 @@ function renderButtons() {
   for (var i = 0; i < cities.length; i++) {
     // Then dynamicaly generates buttons for each city in the array
     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var a = $("<button>");
+    var a = $("<li>");
     // Adds a class of city to our button
-    a.addClass("cities");
+    a.addClass("cities list-group-item");
     // Added a data-attribute
     a.attr("data-name", cities[i]);
-    // Provided the initial button text
-    a.text(cities[i]);
     // Added the button to the buttons-view div
-
+    a.text(cities[i]).css('textTransform', 'capitalize')
     $("#buttons-view").append(a);
   } 
 }    
+
 $("#add-cities").on("click", displayCityInfo, function(event) {
   event.preventDefault();
   // This line of code will grab the input from the textbox
@@ -30,13 +29,67 @@ $("#add-cities").on("click", displayCityInfo, function(event) {
   // The city from the textbox is then added to our array
   cities.push(city);
   // Calling renderButtons which handles the processing of our city array
- 
   storeCities();
   renderButtons();
-
+  addCityDisplay(city);
+  addCityFuture(city);
 });// displaycityInfo function re-renders the HTML to display the appropriate content
-function displayCityInfo() {
 
+function addCityDisplay(city){
+  $("#cities-view").empty()
+  var APIKey = "166a433c57516f51dfab1f7edaed8413"; 
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + 
+  "&appid=" + APIKey;
+
+  // Creates AJAX call for the specific city button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+
+  }).then(function(response) {
+    console.log(response)
+    $("#cities-view").empty()
+    var cityDivEl = $("<div>");
+    cityDivEl.addClass("border border-white")
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    var imgURL = response.weather[0].icon;
+    var city = $("<div>").html("<h3>" + response.name + " " + " " + moment().format('MMMM Do YYYY') + " " + "<img src ='http://openweathermap.org/img/wn/"+ imgURL +"@2x.png'>"+"</h3>");
+    var temperature = $("<div>").text("Temperature: " + tempF.toFixed(2) +" F");
+    var humidity = $("<div>").text( "Humidity: " + response.main.humidity);
+    var description = $("<div>").text( "Description:  " + response.weather[0].description);
+    var wind = $("<div>").text("Wind Speed: " + response.wind.speed);
+    
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    var APIKey = "166a433c57516f51dfab1f7edaed8413"; 
+    var queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon 
+  
+    // Creates AJAX call for the specific city button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+  
+    }).then(function(response) {
+      var UVI = response[0].value
+      var UVIel = $("<div>").text( "UVI: " + UVI);
+      if (UVI => 6){
+      UVIel.addClass("red")
+      }    
+      else if (UVI < 3){
+      UVIel.addClass("green")
+      }
+      else{
+        UVIel.addClass("yellow")
+      }
+
+    cityDivEl.prepend(city, temperature, humidity, wind, description, UVIel);
+// Append the table row to the table body
+    $("#cities-view").append(cityDivEl);
+  })})
+}
+
+function displayCityInfo() {
+  $("#cities-view").empty()
   var APIKey = "166a433c57516f51dfab1f7edaed8413"; 
   var cityName = $(this).attr("data-name");
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + 
@@ -48,29 +101,111 @@ function displayCityInfo() {
     method: "GET"
 
   }).then(function(response) {
+    console.log(response)
+    $("#cities-view").empty()
     var cityDivEl = $("<div>");
-  
+    cityDivEl.addClass("border border-white")
     var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-  $("#cities-view").text(JSON.stringify(response))
-// Methods run on jQuery selectors return the selector they we run on
-// This is why we can create and save a reference to a td in the same statement we update its text
-    var city = $("<div>").html("<h1>" + response.name + " Weather Details</h1>");
+    var imgURL = response.weather[0].icon;
+    var city = $("<div>").html("<h3>" + response.name + " " + " " + moment().format('MMMM Do YYYY') + " " + "<img src ='http://openweathermap.org/img/wn/"+ imgURL +"@2x.png'>"+"</h3>");
     var temperature = $("<div>").text("Temperature: " + tempF.toFixed(2) +" F");
     var humidity = $("<div>").text( "Humidity: " + response.main.humidity);
     var description = $("<div>").text( "Description:" + response.weather[0].description);
     var wind = $("<div>").text("Wind Speed: " + response.wind.speed);
-    var imgURL = response.weather[0].icon;
- // Creating an element to hold the image
-   var image = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+ imgURL +"@2x.png");
-   
+    
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    var APIKey = "166a433c57516f51dfab1f7edaed8413"; 
+    var queryURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon 
+  
+    // Creates AJAX call for the specific city button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+  
+    }).then(function(response) {
+      var UVI = response[0].value
+      var UVIel = $("<div>").text( "UVI: " + UVI);
+      if (UVI => 6){
+      UVIel.addClass("red")
+      }    
+      else if (UVI < 3){
+      UVIel.addClass("green")
+      }
+      else{
+        UVIel.addClass("yellow")
+      }
 
-
-
-// Append the newly created table data to the table row
-    cityDivEl.prepend(city, temperature, humidity, wind, description, image);
+    cityDivEl.prepend(city, temperature, humidity, wind, description, UVIel);
 // Append the table row to the table body
     $("#cities-view").append(cityDivEl);
-    })}
+  })})
+}
+
+function displayFuture() {
+    $("#future-view").empty()
+      var APIKey = "166a433c57516f51dfab1f7edaed8413"; 
+      var cityName = $(this).attr("data-name");
+      var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + 
+      "&appid=" + APIKey;
+    
+      // Creates AJAX call for the specific city button being clicked
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+    
+      }).then(function(response) {        
+        var future = [response.list[6],response.list[14],response.list[22],response.list[30],response.list[38]]
+    
+        for(let i = 0; i < future.length; i++){ 
+        var futureDivEL = $("<div>");
+        futureDivEL.addClass("card text-white bg-primary pb-4 mr-2 p-1 round float-left")
+        futureDivEL.attr("style","max-width: 10rem")
+        var tempF = (future[i].main.temp - 273.15) * 1.80 + 32;
+        var city = $("<div>").html("<p>" + moment(future[i].dt_txt).format('MMMM Do YYYY') + "</p>");
+        var temperature = $("<div>").text("Temperature: " + tempF.toFixed(2) +" F");
+        var humidity = $("<div>").text( "Humidity: " + future[i].main.humidity + "%");
+        var imgURL = future[i].weather[0].icon;
+        var image = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+ imgURL +"@2x.png",).attr("width","50px");
+   
+    futureDivEL.prepend( city, image, temperature, humidity, );
+// Append the table row to the table body
+    $("#future-view").append(futureDivEL)
+
+  }})
+}
+
+function addCityFuture(city){
+  $("#future-view").empty()
+      var APIKey = "166a433c57516f51dfab1f7edaed8413"; 
+      var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + 
+      "&appid=" + APIKey;
+    
+      // Creates AJAX call for the specific city button being clicked
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+    
+      }).then(function(response) {        
+        var future = [response.list[6],response.list[14],response.list[22],response.list[30],response.list[38]]
+    
+        for(let i = 0; i < future.length; i++){ 
+        var futureDivEL = $("<div>");
+        futureDivEL.addClass("card text-white bg-primary pb-4 mr-2 p-1 round float-left")
+        futureDivEL.attr("style","max-width: 10rem")
+        var tempF = (future[i].main.temp - 273.15) * 1.80 + 32;
+        var city = $("<div>").html("<p>" + moment(future[i].dt_txt).format('MMMM Do YYYY') + "</p>");
+        var temperature = $("<div>").text("Temperature: " + tempF.toFixed(2) +" F");
+        var humidity = $("<div>").text( "Humidity: " + future[i].main.humidity + "%");
+        var imgURL = future[i].weather[0].icon;
+        var image = $("<img>").attr("src", "http://openweathermap.org/img/wn/"+ imgURL +"@2x.png",).attr("width","50px");
+   
+    futureDivEL.prepend( city, image, temperature, humidity, );
+// Append the table row to the table body
+    $("#future-view").append(futureDivEL);
+}})
+
+}
 
 function init() {
       // Get stored city from localStorage
@@ -84,9 +219,7 @@ function init() {
     
      
       renderButtons();
-    }
-
-
+}
 
 function storeCities() {
   
@@ -94,8 +227,8 @@ function storeCities() {
 }
 // This function handles events where the add city button is clicked
 
-
 $(document).on("click", ".cities", displayCityInfo);
+$(document).on("click", ".cities", displayFuture);
 
 // Calling the renderButtons function to display the initial buttons
 renderButtons();
